205ab93f4ad0b3f6916ceb29b97c5d725d28ec90
SuiMachine <SuiMachine@users.noreply.github.com>
2022-04-08 20:09:36 +0200

    ffffffffffffffffffffffffff

--- Hacks/VRUIManager.cs
+++ Hacks/VRUIManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 namespace ArtOfRallySuiVR.Hacks
@@ -6,19 +7,36 @@ namespace ArtOfRallySuiVR.Hacks
 	public class VRUIManager : MonoBehaviour
 	{
 		private UnityEngine.Rendering.CompareFunction desiredUIComparison = UnityEngine.Rendering.CompareFunction.Always;
+		//private static Shader customUIShader;
+		//private static Material customUIMaterial;
 
 		void Start()
 		{
+/*			if(customUIShader == null || customUIMaterial == null)
+			{
+				var assetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "zwriteui"));
+				if(assetBundle == null)
+				{
+					Plugin.loggerInstance.LogError("No asset bundle loaded?!");
+					return;
+				}
+
+				customUIShader = assetBundle.LoadAsset<Shader>("CustomUIShader");
+				customUIMaterial = assetBundle.LoadAsset<Material>("UI_UIDefaultZwrite");
+				assetBundle.Unload(false);
+			}
+
 			var canvases = this.GetComponentsInChildren<Canvas>();
 			foreach (var canvas in canvases)
 			{
 				canvas.renderMode = RenderMode.WorldSpace;
-			}
+			}*/
 
 
-			var graphics = GetComponentsInChildren<UnityEngine.UI.Graphic>();
+			var graphics = GetComponentsInChildren<UnityEngine.UI.Graphic>(true);
 			foreach (var graphic in graphics)
 			{
+				//graphic.material = customUIMaterial;
 				Material material = graphic.materialForRendering;
 
 				if (material == null)
@@ -27,33 +45,44 @@ namespace ArtOfRallySuiVR.Hacks
 				material.SetInt("unity_GUIZTestMode", (int)desiredUIComparison);
 			}
 
-			var guiLayer = LayerMask.GetMask("UI");
-			var cameras = Resources.FindObjectsOfTypeAll<Camera>();
-			foreach(var camera in cameras)
+			var images = GetComponentsInChildren<UnityEngine.UI.Image>(true);
+			foreach (var image in images)
 			{
-				var postProcess = camera.GetComponent<PostProcessLayer>();
-				if (postProcess != null)
-				{
-					postProcess.volumeLayer &= ~guiLayer;
-				}
+				//image.material = customUIMaterial;
+				Material material = image.materialForRendering;
+
+				if (material == null)
+					continue;
+
+				material.SetInt("unity_GUIZTestMode", (int)desiredUIComparison);
 			}
 		}
 
+		Canvas[] canvases = null;
+
 		void Update()
 		{
+			if(canvases == null)
+				canvases = this.GetComponentsInChildren<Canvas>(true);
+
 			var flatRotation = Quaternion.Euler(0, Camera.current.transform.eulerAngles.y, 0);
-			var canvases = this.GetComponentsInChildren<Canvas>();
 			foreach (var canvas in canvases)
 			{
-				canvas.renderMode = RenderMode.WorldSpace;
-				canvas.transform.position = Camera.current.transform.position + flatRotation * Vector3.forward * 25;
-				canvas.transform.LookAt(Camera.current.transform);
-				canvas.transform.eulerAngles = new Vector3(0, canvas.transform.eulerAngles.y + 180, 0);
-				canvas.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+				if (canvas != null)
+				{
+					canvas.renderMode = RenderMode.WorldSpace;
+					canvas.transform.position = Camera.current.transform.position + flatRotation * Vector3.forward * 25;
+					canvas.transform.LookAt(Camera.current.transform);
+					canvas.transform.eulerAngles = new Vector3(0, canvas.transform.eulerAngles.y + 180, 0);
+					canvas.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+				}
 			}
 
-			if (this.gameObject.scene.name == "MainMenu")
+/*			if (this.gameObject.scene.name == "MainMenu")
+			{
 				this.enabled = false;
+				return;
+			}*/
 		}
 	}
 }
--- Hacks/VR_Recenter.cs
+++ Hacks/VR_Recenter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 namespace ArtOfRallySuiVR.Hacks
 {
@@ -10,10 +12,14 @@ namespace ArtOfRallySuiVR.Hacks
 		public static List<VR_Recenter> VRCameraInstances = new List<VR_Recenter>();
 		Camera cameraRef;
 		Transform reorientNodeTransform;
+		BeautifyEffect.Beautify beautifyRef;
+
 
 		void Start()
 		{
 			cameraRef = this.GetComponent<Camera>();
+			beautifyRef = this.GetComponent<BeautifyEffect.Beautify>();
+			var postProcess = this.GetComponent<PostProcessVolume>();
 			if(!this.transform.parent.name.StartsWith("VR"))
 			{
 				UnityEngine.XR.InputTracking.disablePositionalTracking = true;
@@ -24,7 +30,7 @@ namespace ArtOfRallySuiVR.Hacks
 				this.reorientNodeTransform = reorientNode.transform;
 				this.transform.SetParent(reorientNodeTransform.transform, true);
 				VRCameraInstances.Add(this);
-				SetForward();
+				StartCoroutine(SetForwardDelayed());
 				if(AwesomeTechnologies.VegetationStudio.VegetationStudioManager.Instance != null)
 				{
 					var vegeSystems = AwesomeTechnologies.VegetationStudio.VegetationStudioManager.Instance.VegetationSystemList;
@@ -36,11 +42,51 @@ namespace ArtOfRallySuiVR.Hacks
 						}
 					}
 				}
+
+				if(postProcess != null)
+				{
+					var profile = postProcess.profile;
+					profile.TryGetSettings<DepthOfField>(out DepthOfField dofSettings);
+
+					if(dofSettings == null)
+					{
+						dofSettings = profile.AddSettings<DepthOfField>();
+					}
+					if(dofSettings != null)
+					{
+						dofSettings.active = true;
+						dofSettings.enabled.overrideState = true;
+						dofSettings.enabled.value = true;
+						dofSettings.aperture.overrideState = true;
+						dofSettings.aperture.value = 100;
+						dofSettings.focusDistance.overrideState = true;
+						dofSettings.focusDistance.value = 140;
+						dofSettings.focalLength.overrideState = true;
+						dofSettings.focalLength.value = 0.5f;
+					}
+				}
 			}
 		}
 
+		void Update()
+		{
+			if(beautifyRef != null)
+			{
+				beautifyRef.depthOfField = false;
+			}
+		}
+
+		public IEnumerator SetForwardDelayed()
+		{
+			yield return null;
+			SetForward();
+		}
+
+
+
 		public void SetForward()
 		{
+			reorientNodeTransform.transform.localPosition = Vector3.zero;
 			reorientNodeTransform.localEulerAngles = new Vector3(0, -this.transform.localEulerAngles.y, 0);
 		}
 
