d67187b965b7aa0033d3aa94adfe34e9e7c38fe3
SuiMachine <SuiMachine@users.noreply.github.com>
2022-04-25 20:13:57 +0200

    Low angle / close camera

---
+++ Source/Hacks/HookPoints/LowCamerasDetour.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace ArtOfRallySuiVR.Hacks.HookPoints
+{
+	/// <summary>
+	/// This replaces some of the cameras, with cameras really close to the car
+	/// </summary>
+	[HarmonyPatch]
+	public class LowCamerasDetour
+	{
+		public static bool Use;
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(CarCameras), "Start")]
+		public static void StartDetour(List<CameraAngle> ___CameraAnglesList)
+		{
+			if(Use)
+			{
+				___CameraAnglesList[(int)CameraAngle.CameraAngles.CAMERA8] = new CameraAngle(8, 3, -2, CameraAngle.CameraAngles.CAMERA8);
+			}
+		}
+	}
+}
--- Source/Plugin.cs
+++ Source/Plugin.cs
@@ -4,31 +4,38 @@ using BepInEx;
 namespace ArtOfRallySuiVR
 {
 	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        public static HarmonyLib.Harmony harmonyInstance;
-        public static BepInEx.Logging.ManualLogSource loggerInstance;
+	public class Plugin : BaseUnityPlugin
+	{
+		public static HarmonyLib.Harmony harmonyInstance;
+		public static BepInEx.Logging.ManualLogSource loggerInstance;
 
-        private void Awake()
-        {
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            loggerInstance = Logger;
-            harmonyInstance = new HarmonyLib.Harmony("local.artofrallysuivr.suicidemachine");
+		private void Awake()
+		{
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			loggerInstance = Logger;
+			harmonyInstance = new HarmonyLib.Harmony("local.artofrallysuivr.suicidemachine");
 
-            if(UnityEngine.XR.XRSettings.loadedDeviceName != "")
+			if (UnityEngine.XR.XRSettings.loadedDeviceName != "")
 			{
 #pragma warning disable CS0618 // Type or member is obsolete
-                Logger.LogInfo($"Initializing VR for {UnityEngine.XR.XRSettings.loadedDeviceName}");
-                UnityEngine.XR.InputTracking.disablePositionalTracking = true;
-                GlobalVRInstancesManager.Init();
-                Logger.LogInfo($"Hotpatching...");
-                harmonyInstance.PatchAll();
+				Logger.LogInfo($"Initializing VR for {UnityEngine.XR.XRSettings.loadedDeviceName}");
+				UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+				GlobalVRInstancesManager.Init();
+				ProcessConfig();
+
+				Logger.LogInfo($"Hotpatching...");
+				harmonyInstance.PatchAll();
 #pragma warning restore CS0618 // Type or member is obsolete
 
-                Logger.LogInfo($"Seems to be OK");
-            }
-            else
-                Logger.LogInfo($"Loaded device is None - ignoring hotpatching.");
-        }
-    }
+				Logger.LogInfo($"Seems to be OK");
+			}
+			else
+				Logger.LogInfo($"Loaded device is None - ignoring hotpatching.");
+		}
+
+		private void ProcessConfig()
+		{
+			Hacks.HookPoints.LowCamerasDetour.Use = Config.Bind("Modifications", "ModifyCameras", false).Value;
+		}
+	}
 }
